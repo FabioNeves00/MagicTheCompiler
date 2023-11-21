bc2dce4a65e970d875258c29dc165a657a67491e
Fabio Neves <fabinhoneves09@gmail.com>
2023-11-21 12:06:54 -0300

    chore: removed end compile value (null)

--- src/compile.ts
+++ src/compile.ts
@@ -3,10 +3,9 @@ import { Parser } from "./modules/parser";
 import { createGlobalEnvironment } from "./modules/runtime/environment";
 import { evaluateProgram } from "./modules/runtime/interpreter";
 
-const data = Bun.file(join(import.meta.dir, Bun.argv[1]))
+const data = Bun.file(Bun.argv[2])
 const source_code = await data.text()
 const evaluated = evaluateProgram(
   new Parser().generateAbstractSyntaxTree(source_code),
   createGlobalEnvironment()
 );
-console.log(evaluated);
